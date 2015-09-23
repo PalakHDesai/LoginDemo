7c4900a18380c6aa404bb72ecf8382511c1536f6
PalakHDesai <palakdesai72525@gmail.com>
2015-09-23 18:02:53 +0530

    First Commit

---
+++ Js/controller.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+var app = angular.module('myApp', ['FirstService']).controller('LoginController', function ($scope, login) {
+    $scope.CheckData = function () {
+        debugger;
+        var userName = $scope.username;
+        var password = $scope.password;
+      
+        var promise = login.CheckData(userName, password);
+        //var promise = checkdata(userName,password);
+        if (promise) {
+            alert('welcome to the hell');
+        } else {
+            alert('invalid user');
+        }
+    }
+});
+
---
+++ Js/service.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+
+angular.module('FirstService', []).factory('login', function() {
+    return {
+        CheckData :function (username,password)
+        {
+        debugger;
+        var response;
+        if (username == "nosql" && password == "html5") {
+            response = true;
+        } else {
+            response = false;
+        }
+        return response;
+    }
+}
+     
+});
